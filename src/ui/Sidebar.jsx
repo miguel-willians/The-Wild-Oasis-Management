8e57fcb2c7d05b83a869b4818c853f3d5171f3cb
miguel-willians <miiguel.wso@gmail.com>
2025-01-01 14:48:58 -0300

    Criando a navegação no Sidebar

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,3 +1,6 @@
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+
 import styled from "styled-components";
 
 const StyledSidebar = styled.aside`
@@ -5,10 +8,18 @@ const StyledSidebar = styled.aside`
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
   grid-row: 1/ -1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function Sidebar() {
-  return <StyledSidebar>Sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
