198bd7240301ebee8834fdd4a0d69b42e16ba0de
miguel-willians <miiguel.wso@gmail.com>
2025-01-01 12:22:15 -0300

    Criando componentes reutilizáveis

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
 function App() {
-  return <div>hi</div>;
+  const StyledApp = styled.main`
+    /* background-color: orangered; */
+    padding: 20px;
+  `;
+
+  // GlobalStyles: Proveniente do styled components, ele é uma variável js que armazena todos os estilos e retorna um componente. Este componente global não pode ter filhos ou receber props.
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            {/* o prop "as" faz com que o styled components renderize a tag passada como valor, ao invés da tag padrão do componente */}
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check-In</Button>
+              <Button variation="secondary" size="small">
+                Check-Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
