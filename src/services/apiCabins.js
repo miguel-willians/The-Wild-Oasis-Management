4c1fa7f6bc90c725e6a5c1b39b161e624943f3d6
miguel-willians <miiguel.wso@gmail.com>
2025-01-04 11:59:37 -0300

    Terminando de configurar o CreateCabinForm

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,5 +1,19 @@
 import supabase from "./supabase";
 
+export default async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.log(error);
+    throw new Error("There was an error while creating the cabin.");
+  }
+
+  return data;
+}
+
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
 
