9ab0d7a5fb8b7d6768246e736463819c3a275dc0
miguel-willians <miiguel.wso@gmail.com>
2025-01-04 14:46:57 -0300

    Tratando erros no React Hook Form

--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const StyledMain = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+  overflow: scroll;
 `;
 
 function Main({ children }) {
