6c3cf47c4ab37c64a8b76b1a909633539c4963b7
miguel-willians <miiguel.wso@gmail.com>
2025-01-16 13:23:02 -0300

    Corrigindo um pequeno bug de autenticação em useLogin

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       // Forma manual de guardar dados no cachê do React Query.
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
 
