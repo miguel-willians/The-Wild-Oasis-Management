8440d06fd3dc47bb027d057ca0fb5fe7dbcba45d
miguel-willians <miiguel.wso@gmail.com>
2025-01-17 14:44:18 -0300

    Construindo a Header da aplicação

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,4 +1,5 @@
-import Logout from "../features/authentication/Logout";
+import UserAvatar from "../features/authentication/UserAvatar";
+import HeaderMenu from "./HeaderMenu";
 
 import styled from "styled-components";
 
@@ -6,12 +7,18 @@ const StyledHeader = styled.header`
   border-bottom: 1 solid var(--color-grey-100);
   padding: 1.2rem 4.8rem;
   background-color: var(--color-grey-0);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
