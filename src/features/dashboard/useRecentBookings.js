a07e23f2f217c0aa095bd17cfb1baa7389ffb6fb
miguel-willians <miiguel.wso@gmail.com>
2025-01-21 17:22:09 -0300

    Computando os dados do DashboardLayout

---
+++ src/features/dashboard/useRecentBookings.js
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+import { getBookingsAfterDate } from "../../services/apiBookings";
+
+export function useRecentBookings() {
+  const [searchParams] = useSearchParams();
+
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+
+  // SubDays é uma função que calcula a subtração de dias. Recebe dois parâmetros, o dia de hoje e os dias que queremos subtrair
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: bookings } = useQuery({
+    queryFn: () => getBookingsAfterDate(queryDate),
+    queryKey: ["bookings", `last-${numDays}`],
+  });
+
+  return { isLoading, bookings };
+}
