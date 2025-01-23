86fa4672a379e41cb790add7d9012f40074992a5
miguel-willians <miiguel.wso@gmail.com>
2025-01-23 11:43:24 -0300

    Criando e mostrando as atividades do dia através de TodayActivity

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -8,6 +8,7 @@ import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
 
 import styled from "styled-components";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -19,7 +20,6 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
   const { bookings, isLoading } = useRecentBookings();
   const {
-    stays,
     confirmedStays,
     isLoading: isLoadingStays,
     numDays,
@@ -37,7 +37,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today's Activities</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
